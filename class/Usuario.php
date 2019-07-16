043f960596fbd73ead58804b709df0324a7a2833
Christian Mota <christian_qlz2012@hotmail.com>
2019-07-16 10:40:11 -0300

    Comando delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -35,12 +35,7 @@ class Usuario{
             ":ID"=>$id
         ));
         if (count($results) > 0 ){
-            $row = $results[0];
-            
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
             
         }
     }
@@ -66,17 +61,61 @@ class Usuario{
             ":PASSWORD"=>$password
         ));
         if (count($results) > 0 ){
-            $row = $results[0];
-            
-            $this->setIdusuario($row['id_usuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
             
         }else{
             throw new Exception("Login ou senha inválidos");
         }
     }
+
+    public function setData($data){
+
+        $this->setIdusuario($data['id_usuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+
+    public function insert(){
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha()
+        ));
+        if (count($results) > 0 ){
+            $this->setData($results[0]);
+            
+        }
+    }
+
+    public function update($login,$password){
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE id_usuario = :ID",array(
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha(),
+            ":ID"=>$this->getIdusuario(),
+        ));
+    }
+    public function __construct($login = "", $password =""){
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+    }
+
+    public function delete(){
+        $sql = new Sql();
+        $sql->query("DELETE FROM tb_usuarios WHERE id_usuario = :ID", array(
+            ":ID"=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
     public function __toString()
     {
         return json_encode(array(
--- index.php
+++ index.php
@@ -13,7 +13,24 @@ require_once("config.php");
 // echo json_encode($result);
 
 //carrega um usuário usando login e senha.
-$root = new Usuario();
-$root->login("root","asdsaas");
-echo $root;
+// $root = new Usuario();
+// $root->login("root","asdsaas");
+// echo $root;
+
+//Criando novo usu
+// $aluno = new Usuario("aluno","@lun0");
+// $aluno->insert();
+// echo $aluno;
+
+//Alterar um usuário
+// $usuario = new Usuario();
+// $usuario->loadById(4);
+// $usuario->update("professor", "12123");
+// var_dump($usuario);
+
+
+$usuario = new Usuario();
+$usuario->loadById(3);
+$usuario->delete();
+echo $usuario;
 ?>
